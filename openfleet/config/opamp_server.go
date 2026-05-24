89ef233ffa16af033ee02d09c1487ade1f3d3ec0
Harpreet Vaid <harpreet798677@gmail.com>
2026-05-24 12:00:10 +0530

    Something Fucked Up

--- openfleet/config/opamp_server.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type OpAMPServerConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
-}
--- openfleet/config/web_server.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type WebServerConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
-}
